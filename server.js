178e716e3699bccfeb68275db74bfcda9ceb1a64
richiej <jumbor6@gmail.com>
2022-01-24 17:13:43 +0100

    Added server base url

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,"./public/build")))
+app.use(express.static(path.join(__dirname, "./build")));
+
+app.get(
+  /\.(js|css|map|ico|json|txt|html)$/,
+  express.static(path.join(__dirname, "./build"))
+)
+
+app.use("/*", (req, res, next) =>{
+  res.send(path.join(__dirname, "./build/index.html"));
+})
 
 // react-scripts start
+app.listen(3001, () => console.log('listening on...3001'));
--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://127.0.0.1:4000/";
+// export const BASE_URL = "http://127.0.0.1:4000/";
+export const BASE_URL = " https://my-seq-server.herokuapp.com/";
 
 export const AUTH = {
   CREATE_USER: BASE_URL + "auth/",
