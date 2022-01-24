912e0dd6e5aba7b280e323e585e3ea61319adabd
richiej <jumbor6@gmail.com>
2022-01-24 20:50:54 +0100

    Commit

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require("express");
-const path = require("path");
+// const express = require("express");
+// const path = require("path");
+// const app = express();
 
-const app = express();
+// app.use(express.static(path.join(__dirname, "./build")));
 
-app.use(express.static(path.join(__dirname, "./build")));
+// app.get(
+//   /\.(js|css|map|ico|json|txt|html)$/,
+//   express.static(path.join(__dirname, "./build"))
+// )
 
-app.get(
-  /\.(js|css|map|ico|json|txt|html)$/,
-  express.static(path.join(__dirname, "./build"))
-)
+// app.use("/*", (req, res, next) =>{
+//   res.send(path.join(__dirname, "./build/index.html"));
+// })
 
-app.use("/*", (req, res, next) =>{
-  res.send(path.join(__dirname, "./build/index.html"));
-})
-
-// react-scripts start
-app.listen(3001, () => console.log('listening on...3001'));
+// // react-scripts start
+// app.listen(3001, () => console.log('listening on...3001'));
